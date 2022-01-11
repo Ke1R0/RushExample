057de1e934f69447dbb170b8dde6b505d711c03e
Artem Bozhko <abozhko@Artems-MacBook-Pro.local>
2022-01-11 14:05:50 +0200

    update package

--- projects/delivery/examples/src/index.tsx
+++ projects/delivery/examples/src/index.tsx
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
+import { App } from "../../src/App";
 ReactDOM.render(<App storeName="test" />, document.getElementById("root"));
--- projects/delivery/webpack.config.js
+++ projects/delivery/webpack.config.js
@@ -1,47 +1,44 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ESLintPlugin =require("eslint-webpack-plugin");
 const path = require("path");
-
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: "./examples/src/index.html",
+});
 module.exports = {
-  entry: "./src/index",
+  // entry: path.join(__dirname, "examples/src/index.tsx"),
+  entry: "./src/index.ts",
   mode: "development",
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     port: 3002,
     historyApiFallback: true,
   },
-  output: {      
-    path: path.join(__dirname, './dist'),      
-    filename: 'main.js',      
-    library: "@infinity/delivery",      
-    libraryTarget: 'umd',      
-    publicPath: '/dist/',      
-    umdNamedDefine: true  
-  },
-  resolve: {     
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom')
-    }
-  },
-  externals: {      
-      // Don't bundle react or react-dom      
-    react: {          
-        commonjs: "react",
-        commonjs2: "react",
-        amd: "React",
-        root: "React"
-    },
-    "react-dom": {
-        commonjs: "react-dom",
-        commonjs2: "react-dom",
-        amd: "ReactDOM",
-        root: "ReactDOM"
-    }
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'index.js'
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
+    // alias: {
+    //   'react': path.resolve(__dirname, './node_modules/react'),
+    //   'react-dom': path.resolve(__dirname, './node_modules/react-dom')
+    // }
   },
+  // externals: {
+  //     // Don't bundle react or react-dom      
+  //   react: {
+  //       commonjs: "react",
+  //       commonjs2: "react",
+  //       amd: "React",
+  //       root: "React"
+  //   },
+  //   "react-dom": {
+  //       commonjs: "react-dom",
+  //       commonjs2: "react-dom",
+  //       amd: "ReactDOM",
+  //       root: "ReactDOM"
+  //   }
+  // },
   module: {
     rules: [
       {
@@ -61,8 +58,6 @@ module.exports = {
     new ESLintPlugin({
       extensions: ["js", "jsx", "ts", "tsx"],
     }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+    htmlPlugin
+  ]
 };
