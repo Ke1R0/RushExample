1e74ab82a0571d0a037c1c7e2e9b461b326d57ec
Artem Bozhko <abozhko@Artems-MacBook-Pro.local>
2022-01-11 14:34:58 +0200

    update package

---
+++ projects/delivery/dist/App.js
@@ -0,0 +1,4 @@
+import React, { Fragment } from 'react';
+export const App = ({
+  storeName
+}) => /*#__PURE__*/React.createElement(Fragment, null, /*#__PURE__*/React.createElement("h1", null, storeName, ": Delivery App"), /*#__PURE__*/React.createElement("p", null, "Description here"));
--- projects/delivery/webpack.config.js
+++ projects/delivery/webpack.config.js
@@ -5,40 +5,16 @@ const htmlPlugin = new HtmlWebpackPlugin({
   template: "./examples/src/index.html",
 });
 module.exports = {
-  // entry: path.join(__dirname, "examples/src/index.tsx"),
-  entry: "./src/index.ts",
+  entry: path.join(__dirname, "examples/src/index.tsx"),
   mode: "development",
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     port: 3002,
     historyApiFallback: true,
   },
-  output: {
-    path: path.join(__dirname, './dist'),
-    filename: 'index.js'
-  },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
-    // alias: {
-    //   'react': path.resolve(__dirname, './node_modules/react'),
-    //   'react-dom': path.resolve(__dirname, './node_modules/react-dom')
-    // }
   },
-  // externals: {
-  //     // Don't bundle react or react-dom      
-  //   react: {
-  //       commonjs: "react",
-  //       commonjs2: "react",
-  //       amd: "React",
-  //       root: "React"
-  //   },
-  //   "react-dom": {
-  //       commonjs: "react-dom",
-  //       commonjs2: "react-dom",
-  //       amd: "ReactDOM",
-  //       root: "ReactDOM"
-  //   }
-  // },
   module: {
     rules: [
       {
