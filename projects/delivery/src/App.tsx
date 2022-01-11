0d8db0182eca788bccf9a58c7cdb83b1ff9a4795
Artem Bozhko <abozhko@Artems-MacBook-Pro.local>
2022-01-11 14:10:27 +0200

    update package

--- projects/delivery/src/App.tsx
+++ projects/delivery/src/App.tsx
@@ -3,6 +3,6 @@ import React, { Fragment } from 'react';
 export const App = ({ storeName }) => (
     <Fragment>
         <h1>{storeName}: Delivery App</h1>
-        <p>Description</p>
+        <p>Description here</p>
     </Fragment>
 );
