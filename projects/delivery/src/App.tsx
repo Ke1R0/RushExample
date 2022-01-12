226282331ede8140ad7a512593a56053e302341d
Artem Bozhko <artem.bozhko@tesco.com>
2022-01-12 15:58:51 +0200

    update description

--- projects/delivery/src/App.tsx
+++ projects/delivery/src/App.tsx
@@ -3,6 +3,6 @@ import React, { Fragment } from 'react';
 export const App = ({ storeName }) => (
     <Fragment>
         <h1>{storeName}: Delivery App</h1>
-        <p>The very first store. Trust us!</p>
+        <p>The very first store. Trust us!!!</p>
     </Fragment>
 );
