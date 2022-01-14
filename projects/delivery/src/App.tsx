4e56c16966c41a7908cddffc86f054ba67382a9b
Artem Bozhko <artem.bozhko@tesco.com>
2022-01-14 11:23:11 +0200

    qwertyss

--- projects/delivery/src/App.tsx
+++ projects/delivery/src/App.tsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 
 export const App = ({ storeName }) => (
     <Fragment>
-        <h1>{storeName}: QWERTY Delivery App</h1>
+        <h1>{storeName}: QWERTYSS Delivery App</h1>
         <p>The very first store.</p>
     </Fragment>
 );
