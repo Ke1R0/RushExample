43d46b4312c5f9db6f438aec7e036d11aea60216
Artem Bozhko <artem.bozhko@tesco.com>
2022-01-14 09:55:15 +0200

    fix bug 01

--- projects/delivery/dist/App.js
+++ projects/delivery/dist/App.js
@@ -1,4 +1,4 @@
 import React, { Fragment } from 'react';
 export const App = ({
   storeName
-}) => /*#__PURE__*/React.createElement(Fragment, null, /*#__PURE__*/React.createElement("h1", null, storeName, ": Releazzz Delivery App"), /*#__PURE__*/React.createElement("p", null, "The very first store."));
+}) => /*#__PURE__*/React.createElement(Fragment, null, /*#__PURE__*/React.createElement("h1", null, storeName, ": Delivery App"), /*#__PURE__*/React.createElement("p", null, "The very first store."));
